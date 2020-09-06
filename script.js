6cacf704bd0221395ddcfe640429dbcad6205bbd
shay_daniel <shay@isom.org>
2020-09-05 17:32:51 -0700

    added functional js page

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
+var militaryTime;
+var timeArray = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+
 getCurrentDay();
-getCurrentTime ();
+getCurrentTime();
+fillColor();
 
 //functions
-function getCurrentDay () { 
+function getCurrentDay() {
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 }
 
-function getCurrentTime () { 
-    $("#currentTime").text(moment().format("hh:mm:ss"));
+function getCurrentTime() {
+    $("#currentTime").text(moment().format("hh:mm"));
+    militaryTime = (moment().format("HH"));
+}
+
+function fillColor() {
+    for (var i = 0; i < timeArray.length; i++) {
+        console.log(document.getElementById("slot" + timeArray[i]));
+        if (militaryTime > timeArray[i]) {
+            $("#slot" + timeArray[i]).addClass("past");
+        } else if (militaryTime == timeArray[i]) {
+            $("#slot" + timeArray[i]).addClass("present");
+
+        } else {
+            $("#slot" + timeArray[i]).addClass("future");
+
+        }
+
+    }
+
 }
 
